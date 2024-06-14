6b5d1ef5cb6ca7d57576c2e7c266d164cb3881f8
유으뜸 <www.goolge.com>
2024-06-14 15:42:47 +0900

    각 요소들 생성 후 자식요소로 넣어줌

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const root = document.getElementById("root");
-console.log(root);
+// console.log(root);
+const header = document.createElement("header");
+// console.log(header);
+const section = document.createElement("section");
+// console.log(section);
+const btn = document.createElement("button");
+console.log(btn);
+
+btn.textContent = "button";
+
+root.appendChild(header);
+root.appendChild(section);
+root.appendChild(btn);
